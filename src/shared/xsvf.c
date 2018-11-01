745e35568eba0aafbc589154bf17b3fd404350b8
Bence Csókás <bence.csokas@gmail.com>
2018-11-01 10:08:24 +0100

    SVF config with libxsvf, failed attempt no. 2.

--- src/shared/xsvf.c
+++ src/shared/xsvf.c
@@ -7,75 +7,107 @@
 #include "libxsvf.h"
 #include "logsys-usb.h"
 
-struct udata{
-	libusb_device_handle* dev;
-	FILE* svfFile;
-	char out_buf[4096];
-	short o_ptr;
-	char chk_buf[4096];
-	char chk_mask[4096];
-	bool tms, chk;
+struct jtag_lines{
+	//0: low, 1: high, -1: keep/don't care
+	char tms, tdi, tdo, mask;
 };
 
-void lsvf_flush_iobuf(struct udata* u){
-	short bytes=(u->o_ptr+7)/8;
-	char paket[4*bytes];
-	
-	//i: byte index in out_buf
-	for(int i=0;i<bytes;i++){
-		int paket_off=u->chk?4*i:2*i;
-		char bits_remain=u->o_ptr-8*i;
-		if(bits_remain>8) bits_remain=8;
+FILE* tmp;
+
+struct udata_s{
+	libusb_device_handle* dev;
+	FILE* svfFile;
+	struct jtag_lines out_buf[4096];
+	short o_ptr;
+	bool cmpMode, wasCmpErr;
+};
+
+void lsvf_write_pack(libusb_device_handle* dev, char bulk_out[1024][4], short bulk_len, bool* wasCmpErr){
+	char raw[4096];
+	for(int i=0;i<bulk_len;i++){
+		memcpy(raw+4*i, bulk_out[i], 4);
+	}
+	libusb_bulk_transfer(dev, LOGSYS_OUT_EP1, raw, 4*bulk_len, NULL, 0);
+	logsys_jtag_check_error(dev, wasCmpErr);
+}
+
+void lsvf_flush_iobuf(struct udata_s* u){
+	bool tmsMode=false;
+	char bulk_out[1024][4]={0};
+	short bulk_len=0;
+	for(int i=0;i<u->o_ptr;i++){
+		struct jtag_lines ln=u->out_buf[i];
+		fprintf(tmp, "TMS:%2d TDI:%2d TDO:%2d MASK:%2d\n", ln.tms, ln.tdi, ln.tdo, ln.mask);
 		
-		if(u->tms) paket[paket_off]=0x40;
-		paket[paket_off]|=bits_remain;
-		for(char x=0;x<bits_remain;x++){
-			paket[paket_off+1]=(paket[paket_off+1]<<1)|(u->out_buf[i]&1);
-			u->out_buf[i]>>1;
-			if(u->chk){
-				paket[paket_off+2]=(paket[paket_off+2]<<1)|(u->chk_buf[i]&1);
-				u->chk_buf[i]>>1;
-				paket[paket_off+3]=(paket[paket_off+3]<<1)|(u->chk_mask[i]&1);
-				u->chk_mask[i]>>1;
+		if(ln.tdi==-1){
+			//TMS write
+			int tms=ln.tms, tcks=0;
+			while(ln.tdi==-1&&ln.tms==tms){
+				i++;
+				tcks++;
+				if(tcks>65536){
+					tcks=65536;
+					break;
+				}
+				if(i<u->o_ptr) ln=u->out_buf[i];
+				else break;
 			}
+			i--;
+			bulk_out[bulk_len][0]=0x20|(0x40*tms);
+			bulk_out[bulk_len][1]=0;
+			bulk_out[bulk_len][2]=(tcks-1)>>8;
+			bulk_out[bulk_len][3]=(tcks-1);
+			bulk_len++;
+		}else{
+			//TDI write
+			char count=0, data=0, dbit=1, chk=0, cmask=0, cbit=1;
+			bool tmsAfter=false;
+			while(ln.tdi!=-1&&!tmsAfter){
+				i++;
+				data&=ln.tdi*dbit;
+				dbit<<=1;
+				if(ln.tdo!=-1){
+					cmask&=cbit;
+					chk&=ln.tdo*cbit;
+				}
+				cbit<<=1;
+				count++;
+				if(ln.tms) tmsAfter=true;
+				if(count==8) break;
+				if(i<u->o_ptr) ln=u->out_buf[i];
+				else break;
+			}
+			i--;
+			bulk_out[bulk_len][0]=(0x40*tmsAfter)|(count-1);
+			bulk_out[bulk_len][1]=data;
+			bulk_out[bulk_len][2]=chk;
+			bulk_out[bulk_len][3]=cmask;
+			bulk_len++;
+		}
+		
+		if(bulk_len==1024){
+			lsvf_write_pack(u->dev, bulk_out, bulk_len, &u->wasCmpErr);
+			bulk_len=0;
 		}
 	}
-	
-	if(u->chk){
-		libusb_bulk_transfer(u->dev, LOGSYS_OUT_EP1, paket, 4*bytes, NULL, 0);
-		bool err;
-		logsys_jtag_check_error(u->dev, &err);
-		if(err){
-			fprintf(stderr, "[FATAL] Compare error!\n");
-			exit(1);
-		}
-	}else{
-		libusb_bulk_transfer(u->dev, LOGSYS_OUT_EP1, paket, 2*bytes, NULL, 0);
-		libusb_bulk_transfer(u->dev, LOGSYS_IN_EP2,  paket, 2*bytes, NULL, 0);
+	if(bulk_len>0){
+		lsvf_write_pack(u->dev, bulk_out, bulk_len, &u->wasCmpErr);
 	}
-}
-
-void lsvf_toggmode(struct udata* u){
-	lsvf_flush_iobuf(u);
-	logsys_jtag_set_mode(u->dev, u->chk?MODE_CMP:MODE_ECHO);
-	u->chk=!(u->chk);
-}
-
-void lsvf_tms(struct udata* u, int tms){
-	if(tms!=u->tms) lsvf_flush_iobuf(u);
-	u->tms=tms;
+	u->o_ptr=0;
 }
 
 int lsvf_host_setup(struct libxsvf_host *h){
 	bool ok;
-	struct udata* u=h->user_data;
-	logsys_tx_jtag_begin(u->dev, u->chk?MODE_CMP:MODE_ECHO, &ok);
+	struct udata_s* u=h->user_data;
+	logsys_tx_jtag_begin(u->dev, MODE_ECHO, &ok);
+	tmp=fopen("/tmp/ls.log", "w");
 	return ok?0:-1;
 }
 
 int lsvf_host_shutdown(struct libxsvf_host *h){
-	struct udata* u=h->user_data;
+	struct udata_s* u=h->user_data;
 	logsys_tx_jtag_end(u->dev);
+	fclose(tmp);
 	return 0;
 }
 
@@ -102,52 +134,48 @@ void lsvf_host_udelay(struct libxsvf_host *h, long usecs, int tms, long num_tck)
 }
 
 int lsvf_host_getbyte(struct libxsvf_host *h){
-	struct udata* u=h->user_data;
+	struct udata_s* u=h->user_data;
 	return fgetc(u->svfFile);
 }
 
 int lsvf_host_pulse_tck(struct libxsvf_host *h, int tms, int tdi, int tdo, int rmask, int sync){
-	struct udata* u=h->user_data;
-	lsvf_tms(u, tms);
-	short o_idx=(u->o_ptr)/8;
-	if((u->chk)!=(tdo!=-1)) lsvf_toggmode(u);
-	if(tdi==-1){
-		//keep last
-		tdi=u->out_buf[o_idx]&1;
-	}
-	u->out_buf[o_idx]=(u->out_buf[o_idx]<<1)|tdi;
-	if(u->chk){
-		u->chk_buf[o_idx]<<=1;
-		u->chk_mask[o_idx]<<=1;
-		if(tdo>=0){
-			u->chk_buf[o_idx]|tdo;
-			u->chk_mask[o_idx]|1;
-		}
-	}
+	struct udata_s* u=h->user_data;
+	u->out_buf[u->o_ptr].tms=tms;
+	u->out_buf[u->o_ptr].tdi=tdi;
+	u->out_buf[u->o_ptr].tdo=tdo;
+	u->out_buf[u->o_ptr].mask=rmask;
 	u->o_ptr++;
-	if(u->o_ptr>=4096) lsvf_flush_iobuf(u);
-	return tdo<0?0:tdo;
+	if(sync||u->o_ptr>=4096) lsvf_flush_iobuf(u);
+	return u->wasCmpErr?-1:1;
 }
 
 void lsvf_host_pulse_sck(struct libxsvf_host *h){}
 
 void lsvf_host_set_trst(struct libxsvf_host *h, int v){}
 
-int lsvf_host_set_frequency(struct libxsvf_host *h, int v){}
+int lsvf_host_set_frequency(struct libxsvf_host *h, int v){
+	return 0;
+}
 
-void lsvf_host_report_tapstate(struct libxsvf_host *h){}
+void lsvf_host_report_tapstate(struct libxsvf_host *h){
+	printf("[%s]\n", libxsvf_state2str(h->tap_state));
+}
 
 void lsvf_host_report_device(struct libxsvf_host *h, unsigned long idcode){}
 
-void lsvf_host_report_status(struct libxsvf_host *h, const char *message){}
+void lsvf_host_report_status(struct libxsvf_host *h, const char *message){
+// 	printf("[STATUS] %s\n", message);
+}
 
-void lsvf_host_report_error(struct libxsvf_host *h, const char *file, int line, const char *message){}
+void lsvf_host_report_error(struct libxsvf_host *h, const char *file, int line, const char *message){
+	fprintf(stderr, "[%s:%d] %s\n", file, line, message);
+}
 
 static int realloc_maxsize[LIBXSVF_MEM_NUM];
 
 static void *lsvf_host_realloc(struct libxsvf_host *h, void *ptr, int size, enum libxsvf_mem which)
 {
-	struct udata_s *u = h->user_data;
+	struct udata_s_s *u = h->user_data;
 	if (size > realloc_maxsize[which])
 		realloc_maxsize[which] = size;
 	
@@ -169,9 +197,9 @@ int logsys_jtag_dl_svf(libusb_device_handle* dev, void* f){
 		.report_status = lsvf_host_report_status,
 		.report_error = lsvf_host_report_error,
 		.realloc = lsvf_host_realloc,
-		.user_data = malloc(sizeof(struct udata))
+		.user_data = malloc(sizeof(struct udata_s))
 	};
-	((struct udata*)h.user_data)->dev=dev;
-	((struct udata*)h.user_data)->svfFile=f;
+	((struct udata_s*)h.user_data)->dev=dev;
+	((struct udata_s*)h.user_data)->svfFile=f;
 	return libxsvf_play(&h, LIBXSVF_MODE_SVF);
 }
